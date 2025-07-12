81baf9c1b418464e9a729b452babf9610794db45
iamdivye <divyejaiswal5@gmail.com>
2025-07-12 06:41:38 +0530

    https://codeforces.com/problemset/problem/2044/A

---
+++ EasyProblem.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class EasyProblem {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner in = new Scanner(System.in);
+		int t=in.nextInt();
+		while(t-- > 0) {
+			int n=in.nextInt();
+			System.out.println(n-1);
+		}
+	}
+
+}
