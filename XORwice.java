2ae72efb6bed00e221e5e71f7d5a212d1492ba14
iamdivye <divyejaiswal5@gmail.com>
2025-08-20 19:19:38 +0530

    https://codeforces.com/problemset/problem/1421/A

---
+++ XORwice.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class XORwice {
+
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		int t=in.nextInt();
+		while(t-->0) {
+			int a=in.nextInt();
+			int b=in.nextInt();
+			System.out.println(a^b);
+		}
+	}
+
+}
