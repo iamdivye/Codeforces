9e34097b9d9f9b6ee1ed5e07a5cd961603e51f87
iamdivye <divyejaiswal5@gmail.com>
2025-10-21 22:11:51 +0530

    https://codeforces.com/problemset/problem/2086/A

---
+++ CloudberryJam.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class CloudberryJam {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt(); 
+        while (t-- > 0) {
+            long n = sc.nextLong(); 
+            System.out.println(2 * n); 
+        }
+    }
+}
