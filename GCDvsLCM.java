81a1b9ea931c97b07dc14a4121cd781727a22e6f
iamdivye <divyejaiswal5@gmail.com>
2026-03-31 23:34:08 +0530

    https://codeforces.com/problemset/problem/1665/A

---
+++ GCDvsLCM.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+public class GCDvsLCM {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+
+        while (t-- > 0) {
+            int n = sc.nextInt();
+
+            int a = 1;
+            int b = n - 3;
+            int c = 1;
+            int d = 1;
+
+            System.out.println(a + " " + b + " " + c + " " + d);
+        }
+
+        sc.close();
+    }
+}
